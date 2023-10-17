6d686d7eff0943b1a8d953bf1dd5fe822e769af0
Shun Nakatani <kaicho8636@gmail.com>
2023-10-18 01:17:12 +0900

    Remove minheap section

--- algorithm.fish
+++ algorithm.fish
@@ -31,58 +31,6 @@ function binary_search
 	return 1
 end
 
-function insert
-	set value $argv[1]
-	set heap $argv[2..]
-	set --append heap $value
-	set i (count $heap)
-	while test $i -gt 1
-		set parent (math floor $i/2)
-		if test $heap[$i] -lt $heap[$parent]
-			set tmp $heap[$i]
-			set heap[$i] $heap[$parent]
-			set heap[$parent] $tmp
-		else
-			break
-		end
-		set i $parent
-	end
-	for item in $heap
-		echo $item
-	end
-end
-
-function deletemin
-	set heap $argv
-	set heap[1] $heap[-1]
-	set --erase heap[-1]
-	set i 1
-	set left 2
-	set right 3
-	while set --query heap[$left]
-		if not set --query heap[$right]
-			set min $left
-		else if test $heap[$left] -lt $heap[$right]
-			set min $left
-		else
-			set min $right
-		end
-		if test $heap[$i] -gt $heap[$min]
-			set tmp $heap[$i]
-			set heap[$i] $heap[$min]
-			set heap[$min] $tmp
-		else
-			break
-		end
-		set i $min
-		set left (math "2*$i")
-		set right (math "2*$i+1")
-	end
-	for item in $heap
-		echo $item
-	end
-end
-
 function heap_sort
     for value in $argv
         set --append heap $value
