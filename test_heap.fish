714304067b05a441eb94a4a97485f3611533221f
Shun Nakatani <kaicho8636@gmail.com>
2023-10-18 01:41:33 +0900

    Format indents

--- test_heap.fish
+++ test_heap.fish
@@ -4,7 +4,7 @@ set test_list (jot -r 10)
 echo (heap_sort $test_list)
 
 for n in 1000 2000 4000 8000
-	echo n=$n
-	set test_list (jot -r $n 1 $n)
-	time heap_sort $test_list>/dev/null
+    echo n=$n
+    set test_list (jot -r $n 1 $n)
+    time heap_sort $test_list>/dev/null
 end
