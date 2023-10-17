0ab01cdf79a5aaab21462a3dd9dae3e59999e0bb
Shun Nakatani <kaicho8636@gmail.com>
2023-10-18 01:02:26 +0900

    Optimize heap_sort

--- algorithm.fish
+++ algorithm.fish
@@ -84,12 +84,46 @@ function deletemin
 end
 
 function heap_sort
-	for item in $argv
-		set heap (insert $item $heap)
-	end
-	while set --query heap[1]
-		echo $heap[1]
-		set heap (deletemin $heap)
-	end
+    for value in $argv
+        set --append heap $value
+        set i (count $heap)
+        while test $i -gt 1
+            set parent (math floor $i/2)
+            if test $heap[$i] -lt $heap[$parent]
+                set tmp $heap[$i]
+                set heap[$i] $heap[$parent]
+                set heap[$parent] $tmp
+            else
+                break
+            end
+            set i $parent
+        end
+    end
+    for value in (seq (count $argv))
+        echo $heap[1]
+        set heap[1] $heap[-1]
+        set --erase heap[-1]
+        set i 1
+        set left 2
+        set right 3
+        while set --query heap[$left]
+            if not set --query heap[$right]
+                set min $left
+            else if test $heap[$left] -lt $heap[$right]
+                set min $left
+            else
+                set min $right
+            end
+            if test $heap[$i] -gt $heap[$min]
+                set tmp $heap[$i]
+                set heap[$i] $heap[$min]
+                set heap[$min] $tmp
+            else
+                break
+            end
+            set i $min
+            set left (math "2*$i")
+            set right (math "2*$i+1")
+        end
+    end
 end
-
